b79aa0ab619c7c39cf291ecff2e90d9c5b63066b
laura-sunshine <readie_rocks@hotmail.co.uk>
2022-10-08 20:04:24 +0100

    Add playRound function

--- script.js
+++ script.js
@@ -10,11 +10,60 @@ function getComputerChoice () {
     
     else {
         return 'paper'
-    }
-
-    
+    }   
     
 }
+
+function playRound(playerSelection, computerSelection) {
+
+    playerSelection.toLowerCase()
+
+    if (playerSelection == "rock") {
+
+        if (computerSelection == "rock") {
+            return "It's a draw"
+        }
+        if (computerSelection == "scissors") {
+            return "You win! Rock beats scissors"
+        }
+        if (computerSelection == "paper") {
+            return "You lose! Paper beats rock"
+        }
+        
+    }
+    if (playerSelection == "paper") {
+
+        if (computerSelection == "rock") {
+            return "You win! Paper beats rock!"
+        }
+        if (computerSelection == "scissors") {
+            return "You lose! Scissors beats paper"
+        }
+        if (computerSelection == "paper") {
+            return "It's a draw"
+        }
+        
+    }
+    if (playerSelection == "scissors") {
+
+        if (computerSelection == "rock") {
+            return "You lose! Rock beats scissors"
+        }
+        if (computerSelection == "scissors") {
+            return "It's a draw"
+        }
+        if (computerSelection == "paper") {
+            return "You win! Scissors beats paper"
+        }
+        
+    }
+
+
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice()
+console.log(playRound(playerSelection, computerSelection));
 
 
 console.log(getComputerChoice());
